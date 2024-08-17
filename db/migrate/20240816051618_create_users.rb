2002c83dc57d5fcf471696dcdb9a3a0a12693032
rlogwood <richard.logwood@gmail.com>
2024-08-17 17:24:33 -0400

    improve layout and fix user schema

--- db/migrate/20240816051618_create_users.rb
+++ db/migrate/20240816051618_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[7.2]
   def change
     create_table :users do |t|
-      t.string :email, unique: true
+      t.string :email
       t.string :password_digest
 
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -16,5 +16,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_16_051618) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
